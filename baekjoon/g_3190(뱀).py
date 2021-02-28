735e57440893edf3ddff54d4c25bfe03b1485035
unknown <dea8307@naver.com>
2021-02-28 14:28:49 +0900

    algo study

--- baekjoon/g_3190(뱀).py
+++ baekjoon/g_3190(뱀).py
@@ -1,66 +1,54 @@
+# 뱀 문제 연습
 import sys
 from collections import deque
 
-def check(direction):
-    if direction == 'L':
-        # 왼쪽
-        return
 
-    elif direction == 'D':
-        pass
-
-def bfs(r, c):
-    # 뱀은 매 초마다 이동
-    # 시작 위치 0,0 에서 1초 늘어났을 때 머리가 0,1 에 위치
-    # 만약에 그 위치에 사과가 있다 ?
-    # 없다 ?
-    dr = [0, 1, 0, -1]
-    dc = [1, 0, -1, 0]
-    snake = deque()
-    snake.append((r, c))
-    time = 1
-    d = 0
+def check():
+    dr = [-1, 0, 1, 0]  # 시계 방향
+    dc = [0, 1, 0, -1]
+    snakes = deque()
+    snakes.append((0, 0))
+    nd = 1
+    nr, nc = 0, 0
+    matrix[nr][nc] = 1
+    t = 1
+    # 머리랑 꼬리 생각해야함
+    # 뱀의 길이를 queue에 담아서 확인해준다
+    # 맨 처음은 꼬리, 맨 뒤는 머리로 생각하고
     while True:
-        r, c = r + dr[d], c + dc[d]
-        if 0 <= r < N and 0 <= c < N and matrix[r][c] != 2:
-            if matrix[r][c] == 1: # 사과를 만났을때
-                snake.append((r, c))
-                matrix[r][c] = 2
-
-            # 꼬리가 윽시 길수도있는디
-            elif matrix[r][c] == 0: # 사과 x
-                matrix[r][c] = 2
-                snake.append((r,c))
-                tailr, tailc = snake.popleft()
-                matrix[tailr][tailc] = 0
-
-            if time in snake_dir.keys():
-                if snake_dir[time] == 'L':
-                    d = (d+3) % 4
+        nr, nc = nr + dr[nd], nc + dc[nd]
+        if 0 <= nr < N and 0 <= nc < N and matrix[nr][nc] != 1:
+            if matrix[nr][nc] == 2:  # 사과가 있다면
+                # 그칸에 있떤 사과가 없어지고 꼬리는 움직이지 않는다.
+                matrix[nr][nc] = 1
+                snakes.append((nr, nc))
+            else:
+                matrix[nr][nc] = 1
+                snakes.append((nr, nc))
+                tailf, taile = snakes.popleft()
+                matrix[tailf][taile] = 0
+            if t in snake_dir:
+                if snake_dir[t] == 'L':
+                    nd = (nd - 1) % 4
                 else:
-                    d = (d+1) % 4
-            time += 1
+                    nd = (nd + 1) % 4
+            t += 1
         else:
-            # 밖을 벗어났을 때
-            return time
-
-
+            # 범위가 넘어가거나 자신과 부딪힌다면
+            return t
 
 
 N = int(sys.stdin.readline())  # 보드의 크기
 K = int(sys.stdin.readline())  # 사과의 개수
 matrix = [[0] * N for _ in range(N)]
-# apples = []
 for _ in range(K):
     f, e = map(int, sys.stdin.readline().split())
-    # 이런 말이어디숨
-    matrix[f-1][e-1] = 1
+    # 맨위 좌측이 1행 1열이라 했으므로
+    matrix[f - 1][e - 1] = 2  # 사과는 2
 
-L = int(sys.stdin.readline())  # 뱀 방향 변환 횟수
+L = int(sys.stdin.readline())
 snake_dir = {}
 for _ in range(L):
     time, dir = sys.stdin.readline().split()
     snake_dir[int(time)] = dir
-
-matrix[0][0] = 2
-print(bfs(0, 0))
+print(check())
